f8f14eedf291a024e21686b4805faf462095ac4e
Jinhyun, Nam <jlj4348@naver.com>
2018-09-06 13:52:47 +0900

    Update constants.h

--- opencl_version/constants.h
+++ opencl_version/constants.h
@@ -1,2 +1,16 @@
 
 #define   SUCCESS   0
+
+// api.c ERRORS
+#define   GET_PLATFORM_ERR  0x010
+#define   GET_DEVICE_NUM_ERR  0x011
+#define   GET_DEVICE_ID_ERR   0x012
+#define   ALLOC_ERR   0x013
+#define   CREATE_CONTEXT_ERR  0x014
+#define   CREATE_QUEUE_ERR  0x015
+
+// hints
+#define   SORT  0x100
+#define   COL   0x101
+#define   ROW   0x102
+#define   MAT   0x103

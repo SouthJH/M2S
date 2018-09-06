fee1af269f5f2049916e3a0b4a23c9d699eec47b
Jinhyun, Nam <jlj4348@naver.com>
2018-09-06 12:48:25 +0900

    Create api.c

---
+++ opencl_version/api.c
@@ -0,0 +1,13 @@
+
+
+int m2s_clGetPlatformIDs();
+int m2s_clGetDeviceIDs();
+int m2s_clCreateContext();
+int m2s_clCreateCommandQueue();
+int m2s_clCreateKernel();
+int m2s_clCreateBuffer();
+int m2s_clEnqueueWriteBuffer();
+int m2s_clEnqueueReadBuffer();
+int m2s_clEnqueueCopyBuffer();
+int m2s_clSetKernelArg();
+int m2s_EnqueueNDRangeKernel();
